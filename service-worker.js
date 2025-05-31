f96c9b5de9b817809ab790a49fcf4b5163d5373c
s20owen <71678206+s20owen@users.noreply.github.com>
2025-05-31 18:55:36 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,25 @@
+const CACHE_NAME = 'tdgame-cache-v3';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/main.js',
+  '/style.css',
+  '/manifest.json' // include all needed assets
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => caches.delete(cache))
-      );
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
